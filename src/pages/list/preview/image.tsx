898174c309bdd36b892d2d1ed0b9c23d7ba829ba
Chris <42108459+impossible98@users.noreply.github.com>
2022-02-05 16:40:27 +0800

    feat: init project

--- src/pages/list/preview/image.tsx
+++ src/pages/list/preview/image.tsx
@@ -1,28 +1,48 @@
 import { Center, Image, Spinner } from '@chakra-ui/react';
-import React, { useContext, useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import * as React from 'react';
 import useFileUrl from '../../../hooks/useFileUrl';
 import { FileProps, IContext } from '../context';
 
 export const type = 6;
 export const exts = [];
 
-const ImagePreview = ({ file }: FileProps) => {
+function ImagePreview({ file }: FileProps) {
     const fileUrl = useFileUrl();
-    const [url, setUrl] = useState('');
-    const { getSetting, password } = useContext(IContext);
-    useEffect(() => {
+    const [url, setUrl] = React.useState('');
+    const { getSetting, password } = React.useContext(IContext);
+    React.useEffect(() => {
         setUrl(fileUrl(file));
     }, []);
     return (
-        <Center className='image-box' w='full'>
-            <Image
-                maxH='75vh'
-                fallback={<Spinner color={getSetting('icon color') || '#1890ff'} size='xl' />}
-                rounded='lg'
-                src={url}
-            />
-        </Center>
+        // <Center className='image-box' w='full'>
+        //     <Image
+        //         maxH='75vh'
+        //         fallback={<Spinner color={getSetting('icon color') || '#1890ff'} size='xl' />}
+        //         rounded='lg'
+        //         src={url}
+        //     />
+        // </Center>
+
+        <Box>
+            <ImageList
+                sx={{
+                    borderRadius: '10px',
+                    width: '80%',
+                }}
+                cols={1}
+            >
+                <ImageListItem>
+                    <img
+                        src={url}
+                        loading='lazy'
+                    />
+                </ImageListItem>
+            </ImageList>
+        </Box>
     );
-};
+}
 
 export default ImagePreview;
