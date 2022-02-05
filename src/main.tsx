f105777101e74bb708fb96efd464112bbbb11c54
Chris <42108459+impossible98@users.noreply.github.com>
2022-02-05 15:02:31 +0800

    feat: init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import CssBaseline from '@mui/material/CssBaseline';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import CssBaseline from '@mui/material/CssBaseline';
 
 import App from './App';
 import './i18n';
 import theme from './theme';
-// import Overlay from './components/overlay'
 
 ReactDOM.render(
     <React.StrictMode>
